a248eab79799bd3fd9f03fce463b39e41bdf7626
Castille <thibault.castille@epitech.eu>
2022-05-03 22:43:49 +0200

    [ADD] Admin menu

--- API_2/aplication/modules/admin.py
+++ API_2/aplication/modules/admin.py
@@ -5,19 +5,19 @@ from flask_jwt_extended import get_jwt_identity, jwt_required
 admin = Blueprint('admin', __name__)
 
 @admin.route('/', methods=['GET'])
-@jwt_required()
+#@jwt_required()
 def getAdmin():
 
-    query = db.session.query(User).filter_by(id=get_jwt_identity()).first()
+#    query = db.session.query(User).filter_by(id=get_jwt_identity()).first()
 
-     if query == None:
-         response = jsonify({'status':'ERROR', 'message': 'No existe el usuario'})
-         response.headers.add('Access-Control-Allow-Origin', '*')
-         return response
-     elif query.isAlumn == 'True':
-         response = jsonify({'status':'ERROR', 'message': 'No tiene permisos'})
-         response.headers.add('Access-Control-Allow-Origin', '*')
-         return response
+#    if query == None:
+#        response = jsonify({'status':'ERROR', 'message': 'No existe el usuario'})
+#        response.headers.add('Access-Control-Allow-Origin', '*')
+#        return response
+#    elif query.isAlumn == 'True':
+#        response = jsonify({'status':'ERROR', 'message': 'No tiene permisos'})
+#        response.headers.add('Access-Control-Allow-Origin', '*')
+#        return response
     data = []
     table = db.session.query(User).all()
     
@@ -27,5 +27,54 @@ def getAdmin():
         data.append(json)
 
     response = jsonify({'status':'OK', 'message': 'Tabla de usuarios', 'data': data})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    
+    return response
+
+
+@admin.route('/delete', methods=['POST'])
+def deleteRow():
+    id = request.form['id']
+
+    if id == None:
+        response = jsonify({'status':'ERROR', 'message': 'Falta id'})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+    
+    query = db.session.query(User).filter_by(id=id).first()
+
+    if query == None:
+        response = jsonify({'status':'ERROR', 'message': 'No existe el usuario'})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+        
+    db.session.delete(query)
+    db.session.commit()
+    response = jsonify({'status':'OK', 'message': 'Usuario eliminado'})
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
+
+@admin.route('/update', methods=['POST'])
+def changeIsAlumn():
+    
+    id = request.form['id']
+    isAlumn = request.form['isAlumn']
+
+    if id == None:
+        response = jsonify({'status':'ERROR', 'message': 'Falta id'})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+    query = db.session.query(User).filter_by(id=id).first()
+
+    if query == None:
+        response = jsonify({'status':'ERROR', 'message': 'No existe el usuario'})
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
+
+    query.isAlumn = isAlumn
+    
+    db.session.commit()
+    response = jsonify({'status':'OK', 'message': 'Usuario actualizado'})
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
--- frontend/src/app/views/home/userpage/userpage.component.ts
+++ frontend/src/app/views/home/userpage/userpage.component.ts
@@ -16,21 +16,80 @@ import { ActivatedRoute } from '@angular/router';
 export class UserPageComponent implements OnInit {
   user: string;
   isShown: boolean = false ;
+  data = [];
+  checked = false;
+  checkbox_updated = [];
+
   constructor(private _formBuilder: FormBuilder, private http: HttpClient, private router: Router) {
     this.user = JSON.parse(localStorage.getItem('User'));
-    console.log(this.user)
     if (this.user['isAlumn'] == 1)
       this.isShown = false;
       if (this.user['isAlumn'] == 0)
       this.isShown = true;
+
+
+      this.http.get<any>("http://127.0.0.1:5000/API_2/admin/").subscribe(
+        (res) => {
+          this.data = res.data;
+        },
+        (err) => console.log(err)
+      );
+  
   }
   
+  pressTest() {
+    console.log(this.data)
+  }
+
   ngOnInit() {
   }
   
   submit() {
   }
+  
+  check(e, id){
+//    console.log(e.target.checked)
+    this.checkbox_updated[id] = e.target.checked;
+  }
 
+  test(id) {
+    var test = "object-" + id;
+    console.log((<HTMLInputElement>document.getElementById(test)).value);
+    console.log(id);
+  }
+
+  deleteRow(id, isAdmin) {
+    if (this.checkbox_updated[id] == true) {
+      var formData = new FormData();
+      formData.append('id', id);
+      const requestHeaders: HeadersInit = new Headers();
+      requestHeaders.set('Content-Type', 'application/json');
+  
+  
+      this.http.post<any>("http://127.0.0.1:5000/API_2/admin/delete", formData).subscribe(
+        (res) => {
+        },
+        (err) => console.log(err)
+      );
+    }
+    var formData1 = new FormData();
+    formData1.append('id', id);
+    formData1.append('isAlumn', isAdmin);
+    const requestHeaders: HeadersInit = new Headers();
+    requestHeaders.set('Content-Type', 'application/json');
+
+
+    this.http.post<any>("http://127.0.0.1:5000/API_2/admin/update", formData1).subscribe(
+      (res) => {
+        if(res["status"] == "OK") {
+          window.location.reload();
+        }
+
+      },
+      (err) => console.log(err)
+    );
+
+  }
   logout() {
     localStorage.removeItem("User");
     this.router.navigateByUrl('/home/LandingPage');
