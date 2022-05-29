f879eedb20f26d4440f1d789f38fc50f360774c2
Alejandro Garcia Cancer <alejandro.cancer@live.u-tad.com>
2022-05-30 01:42:05 +0200

    Registro empresas backend

--- API_2/models.py
+++ API_2/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from sqlalchemy import Column, Integer, String, Boolean, DateTime, Time, ForeignKey
 from flask import current_app
 
@@ -17,5 +18,22 @@ class Token(db.Model):
     __tablename__ = 'Tokens'
     token = Column(String(128), nullable=False, primary_key=True)
     user_id = Column(Integer, ForeignKey('Users.id'), nullable=False)
+
+# Crear tabla empresas
+class Company(db.Model):
+    __tablename__ = 'Companies'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False, unique=True)
+    contactPersonName = Column(String(50), nullable=False)
+    contactPhone = Column(String(20))
+    contactEmail = Column(String(50))
+    address = Column(String(100))
+    city = Column(String(50))
+    province = Column(String(50))
+    postalCode = Column(String(10))
+    country = Column(String(50))
+    website = Column(String(50))
+    logo = Column(String(50), default='default.png')
+    lookingForCandidates = Column(String(10))
 
 db.create_all()
