f8b9bd88dd80e2d6af9eb83de46444eab6904192
Castille <thibault.castille@epitech.eu>
2022-05-29 20:45:12 +0200

    Update Page USer

--- frontend/src/app/views/home/userpage/userpage.component.ts
+++ frontend/src/app/views/home/userpage/userpage.component.ts
@@ -1,99 +1,81 @@
-import { Component, OnInit, Injectable} from '@angular/core';
+import { AfterViewInit, Component, ChangeDetectionStrategy, ChangeDetectorRef, OnInit, Input } from '@angular/core';
+import { NguCarousel, NguCarouselConfig } from '@ngu/carousel';
+import { MatBottomSheet } from '@angular/material/bottom-sheet';
+import { BottomSheetShareComponent } from '../../../shared/components/bottom-sheet-share/bottom-sheet-share.component';
 import { Router } from '@angular/router';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { from } from 'rxjs';
-import { ActivatedRoute } from '@angular/router';
 
-@Injectable({
-  providedIn: 'root'
-})
+
 @Component({
   selector: 'app-userpage',
   templateUrl: './userpage.component.html',
   styleUrls: ['./userpage.component.scss']
 })
 export class UserPageComponent implements OnInit {
-  user: string;
-  isShown: boolean = false ;
-  data = [];
-  checked = false;
-  checkbox_updated = [];
-  token = localStorage.getItem("Token");
-
-  constructor(private _formBuilder: FormBuilder, private http: HttpClient, private router: Router) {
-    this.user = JSON.parse(localStorage.getItem('User'));
-    if (this.user['isAlumn'] == 1)
-      this.isShown = false;
-      if (this.user['isAlumn'] == 0)
-      this.isShown = true;
-
-
-      this.http.get<any>("http://thibaultcastille.pythonanywhere.com/API_2/admin/?jwt=" + this.token).subscribe(
-        (res) => {
-          this.data = res.data;
-        },
-        (err) => console.log(err)
-      );
+  @Input('backgroundGray') public backgroundGray;
   
+  public carouselOptions: NguCarouselConfig;
+  public portfolios = [{
+    photo: 'assets/images/sq-10.jpg',
+    text: `Adipisci quas repellat sed. Quasi quaerat aut nam possimus 
+    vitae dignissimos, sapiente est atque tenetur`,
+    title: 'Project One',
+  }, {
+    photo: 'assets/images/sq-11.jpg',
+    text: `Adipisci quas repellat sed. Quasi quaerat aut nam possimus 
+    vitae dignissimos, sapiente est atque tenetur`,
+    title: 'Project Two',
+  }, {
+    photo: 'assets/images/sq-12.jpg',
+    text: `Adipisci quas repellat sed. Quasi quaerat aut nam possimus 
+    vitae dignissimos, sapiente est atque tenetur`,
+    title: 'Project Three',
+  }, {
+    photo: 'assets/images/sq-13.jpg',
+    text: `Adipisci quas repellat sed. Quasi quaerat aut nam possimus 
+    vitae dignissimos, sapiente est atque tenetur`,
+    title: 'Project Four',
+  }, {
+    photo: 'assets/images/sq-15.jpg',
+    text: `Adipisci quas repellat sed. Quasi quaerat aut nam possimus 
+    vitae dignissimos, sapiente est atque tenetur`,
+    title: 'Project Five',
+  }, {
+    photo: 'assets/images/sq-16.jpg',
+    text: `Adipisci quas repellat sed. Quasi quaerat aut nam possimus 
+    vitae dignissimos, sapiente est atque tenetur`,
+    title: 'Project Six',
+  }]
+  
+  constructor(private bottomSheet: MatBottomSheet, private _cdr: ChangeDetectorRef,private router: Router) { 
   }
-  
-  pressTest() {
-    console.log(this.data)
+
+  openShareComponent(): void {
+    this.bottomSheet.open(BottomSheetShareComponent);
   }
 
   ngOnInit() {
-  }
-  
-  submit() {
-  }
-  
-  check(e, id){
-//    console.log(e.target.checked)
-    this.checkbox_updated[id] = e.target.checked;
-  }
-
-  test(id) {
-    var test = "object-" + id;
-    console.log((<HTMLInputElement>document.getElementById(test)).value);
-    console.log(id);
-  }
-
-  deleteRow(id, isAdmin) {
-    if (this.checkbox_updated[id] == true) {
-      var formData = new FormData();
-      formData.append('id', id);
-      const requestHeaders: HeadersInit = new Headers();
-      requestHeaders.set('Content-Type', 'application/json');
-  
-  
-      this.http.post<any>("http://thibaultcastille.pythonanywhere.com/API_2/admin/delete", formData).subscribe(
-        (res) => {
-        },
-        (err) => console.log(err)
-      );
-    }
-    var formData1 = new FormData();
-    formData1.append('id', id);
-    formData1.append('isAlumn', isAdmin);
-    const requestHeaders: HeadersInit = new Headers();
-    requestHeaders.set('Content-Type', 'application/json');
-
-
-    this.http.post<any>("http://thibaultcastille.pythonanywhere.com/API_2/admin/update", formData1).subscribe(
-      (res) => {
-        if(res["status"] == "OK") {
-          window.location.reload();
-        }
-
+    this.carouselOptions = {
+      grid: { xs: 1, sm: 2, md: 3, lg: 3, all: 0 },
+      slide: 2,
+      speed: 400,
+      interval: {timing: 4000},
+      point: {
+        visible: true
       },
-      (err) => console.log(err)
-    );
-
+      load: 2,
+      touch: true,
+      loop: true
+    }
   }
+
+  ngAfterViewInit() {
+    this._cdr.detectChanges();
+  }
+
   logout() {
     localStorage.removeItem("User");
     this.router.navigateByUrl('/home/LandingPage');
   }
   
+
 }
